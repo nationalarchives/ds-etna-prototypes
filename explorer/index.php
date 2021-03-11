e4e048c79e8cf54fa61e8c0d5e1028bc71eb2128
Gwyn Jones <m@gwnjns.com>
2021-03-11 16:16:13 +0000

    Add home page (interim commit)

--- explorer/index.php
+++ explorer/index.php
@@ -2,7 +2,9 @@
 <main>
     <?php require_once '../includes/header-and-nav-bar.php' ?>
     <div class="container">
-
+        <?php require_once('../includes/home-page-cards.php'); ?>
+        <?php require_once('../includes/popular-today.php'); ?>
+        <?php require_once('../includes/featured.php'); ?>
     </div>
 </main>
 <?php require_once '../includes/footer.php' ?>
---
+++ includes/featured.php
@@ -0,0 +1,9 @@
+<div class="row mt-3">
+    <h2>Featured</h2>
+    <p class="lead">Black history month</p>
+</div>
+<div class="row">
+    <div class="col">
+        <img class="card-img-top" src="https://via.placeholder.com/320x180" alt="Card image cap">
+    </div>
+</div>
