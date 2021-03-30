f44ee820d96b8efe3b05be379a08e98950a40171
AshTNA <ashleyaburnett@gmail.com>
2021-03-30 13:13:33 +0100

    Remove tab/search from explorer home

--- includes/explorer/home-page-cards.php
+++ includes/explorer/home-page-cards.php
@@ -6,14 +6,6 @@
                 <li class="breadcrumb-item active" aria-current="page">Explore</li>
             </ol>
         </nav>
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active lead" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true"><strong>Explore the collection</strong></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link lead" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false"><strong>Search</strong></a>
-            </li>
-        </ul>
         <div class="tab-content" id="myTabContent">
             <div class="row mt-4">
                 <p>Select an entry point to explore the collection.</p>
