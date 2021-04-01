7ca66d83f6c65e65138d9a573126c5ef95d1f509
AshTNA <ashleyaburnett@gmail.com>
2021-04-01 14:56:15 +0100

    Change shakespeare to dickens

--- explorer/index.php
+++ explorer/index.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Etna prototypes</h1>
+                <h1>Explorer iterations</h1>
                 <ul>
                     <li><a href="/explorer/v1">V1</a></li>
                     <li><a href="/explorer/v2">V2</a></li>
