b4b2f7cd4f045141697bf6352dde714679dbeef3
Gwyn Jones <m@gwnjns.com>
2021-05-27 13:30:40 +0100

    Add Insights Page

---
+++ insights-page/insights/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<?php require '../includes/head.php' ?>
+
+<body>
+
+<?php require '../includes/header.php' ?>
+
+<main id="maincontent">
+    <?php require '../includes/generic_hero.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" id="content">
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require '../includes/footer.php' ?>
+
+</body>
+</html>
+
+
