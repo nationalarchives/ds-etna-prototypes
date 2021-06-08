0521c0183050e95c1050d261f73e30212989c744
JamesWChan <jwchan1993@gmail.com>
2021-06-08 14:34:43 +0100

    Fix links

--- insights-page/insights-shakespeare-v3.php
+++ insights-page/insights-shakespeare-v3.php
@@ -20,61 +20,6 @@
         </div>
     </div>
 
-    <!--<nav class="chapter-nav">
-        <div class="row">
-            <div class="col-md-8">
-                <p class="mb-0 mt-4">
-                    You are reading <span id="chapter">Career</span> in <span id="article">William Shakespeare's will</span>.
-                </p>
-            </div>
-            <div id="contents-toggle" class="col-md-4">
-                <button>
-                    Contents
-                </button>
-            </div>
-            <div id="contents">
-                <div class="list-title col-md-12">
-                    <p>Contents</p>
-                </div>
-                <ul>
-                    <li>
-                        <a href="#content-1">1. Career</a>
-                    </li>
-                    <li>
-                        <a href="#content-2">2. The Face of Shakespeare</a>
-                    </li>
-                    <li>
-                        <a href="#content-3">3. Content block</a>
-                    </li>
-                    <li>
-                        <a href="#content-4">4. Call out</a>
-                    </li>
-                    <li>
-                        <a href="#content-5">5. Shakespeare and James I</a>
-                    </li>
-                    <li>
-                        <a href="#content-6">6. A Tourist's Guide to Shakespeare's London</a>
-                    </li>
-                    <li>
-                        <a href="#content-7">7. Content block</a>
-                    </li>
-                    <li>
-                        <a href="#content-8">8. Research Resources</a>
-                    </li>
-                    <li>
-                        <a href="#content-9">9. Shakespeare's Will</a>
-                    </li>
-                    <li>
-                        <a href="#content-10">10. Quote</a>
-                    </li>
-                    <li>
-                        <a href="#content-11">11. Find out more</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>-->
-
     <!-- TOP BANNER -->
     <div class="background no-padding">
 
@@ -144,7 +89,7 @@
                     <h2 class="author-name no-margin-bottom">Authored by Dr Jo Pugh</h2>
                     <p class="author-occupation">Principal Records Specialist &mdash; The National Archives, UK</p>
                     <p class="author-info">Jo Pugh is the Principal Records Specialist at The National Archives and is an expert in The National Archives’ records relating to Shakespeare.</p>
-                    <a class="button-inverted" href="">Read more about Jo</a>
+                    <a class="button-inverted" href="https://www.nationalarchives.gov.uk/about/our-research-and-academic-collaboration/our-research-and-people/staff-profiles/dr-jo-pugh/" target="_blank" rel="noopener noreferrer">Read more about Jo</a>
                 </div>
             </div><!-- end row -->
         </div><!-- end container -->
@@ -157,6 +102,7 @@
             <p><span class="stand-first-heading">William Shakespeare is often thought of as one of the greatest writers in the English language.</span></p>
             <p>His plays have been translated into every major language, and are performed more often than any other playwright. Shakespeare’s writing also affected the way the English language evolved, and several words and phrases, such as ‘all’s well that ends well’, ‘with bated breath’ and ‘a foregone conclusion’ have moved into everyday use.</p>
 
+            <span id="content-1"></span>
             <h2>Shakespeare's career</h2>
             <p>William Shakespeare, also known as the ‘Bard’, was born in Stratford-upon-Avon on April 23rd 1564. It is thought he married his wife, Anne Hathaway, in 1582, although we have no specific marriage certificate. He pursued a career as an actor, poet and dramatist in London.</p>
             <p>His now famous plays were performed widely during his lifetime, often at the purpose-built Globe Theatre in London on the south bank of the Thames. The first folio was published in 1623, with 154 sonnets, 37 plays, and 2 long poems. It is suggested that his friends put it together in case others tried to copy Shakespeare’s work and claim it as their own.</p>
@@ -167,6 +113,7 @@
 
 
     <!-- IMAGE VIEWER 1 -->
+    <span id="content-2"></span>
     <div class="row">
 
         <!-- PART 1: IMAGE -->
@@ -194,7 +141,8 @@
                 <p class="caption-text">Date created: 1958</p>
                 <p>The portrait shows a balding man, dark haired, rather askew of eye, and wearing a white lace collar of a type familiar to anyone who has read anything about William Shakespeare. The image itself is quite easily explained. It is a pencil copy, made in the 19th century by an antiquarian and archivist, of a 17th century portrait. But is this an image of Shakespeare, and why is it in the collections of The National Archives at all?</p>
                 <p>The expert among whose papers it was found was Rawdon Lubbock Brown who, as you may have read in a blog earlier this year, was among the staff of the then-Public Record Office… but in Venice.</p>
-                <a class="button" href="">View in the catalogue</a><a class="button" href="">Read more in our blog</a>
+                <a class="button" href="http://discovery.nationalarchives.gov.uk/details/r/C3604789" target="_blank" rel="noopener noreferrer">View in the catalogue</a>
+                <a class="button" href="https://blog.nationalarchives.gov.uk/face-william-shakespeare/" target="_blank" rel="noopener noreferrer">Read more in our blog</a>
             </div><!-- end container -->
         </div><!-- end background -->
 
@@ -212,6 +160,7 @@
 
 
     <!-- RECORD EMBED -->
+    <span id="content-3"></span>
     <div class="background bg-blue">
         <div class="container call-out-box">
             <div class="row">
@@ -219,15 +168,15 @@
 
                 <div class="call-out-links">
                     <ul>
-                        <li><a href=""><span class="heading">The case of Belott versus Mountjoy</span>
+                        <li><a href="http://discovery.nationalarchives.gov.uk/details/r/C5904894" target="_blank" rel="noopener noreferrer"><span class="heading">The case of Belott versus Mountjoy</span>
                                 <p class="no-margin-bottom">This concerns a marriage dowry and contains Shakespeare’s signed deposition. The documents pinpoint his residence in 1604.</p>
                             </a>
                         </li>
-                        <li><a href=""><span class="heading">The case of Keysar versus Burbage and others</span>
+                        <li><a href="http://discovery.nationalarchives.gov.uk/details/r/C2677795" target="_blank" rel="noopener noreferrer"><span class="heading">The case of Keysar versus Burbage and others</span>
                                 <p class="no-margin-bottom">This describes the King’s Men acting company’s involvement in the Blackfriars Playhouse.</p>
                             </a>
                         </li>
-                        <li><a href=""><span class="heading">The case of Witter versus Heminges and Condell</span>
+                        <li><a href="http://discovery.nationalarchives.gov.uk/details/r/C5904893" target="_blank" rel="noopener noreferrer"><span class="heading">The case of Witter versus Heminges and Condell</span>
                                 <p class="no-margin-bottom">This describes the way in which the shares in the syndicate that leased the Globe playhouse were distributed.</p>
                             </a>
                         </li>
@@ -240,6 +189,7 @@
 
 
     <!-- RECORD EMBED -->
+    <span id="content-4"></span>
     <div class="container standard-padding margin-top-medium">
         <div class="row">
             <div class="panel-center">
@@ -250,13 +200,14 @@
                 <h2>Shakespeare and James I</h2>
                 <p class="caption-text bold-text">Reference: LC 2/4/5</p>
                 <p class="margin-top-small">Other documents at The National Archives reveal the growing status and prestige of Shakespeare, as, for example, the 1604 Progress of James I through the city of London, which mentions a grant of cloth to Shakespeare.</p>
-                <a class="button-inverted" href="">View in the catalogue</a>
+                <a class="button-inverted" href="http://discovery.nationalarchives.gov.uk/details/r/C213527 " target="_blank" rel="noopener noreferrer">View in the catalogue</a>
             </div><!-- end panel-center-->
         </div><!-- end row -->
     </div><!-- end container -->
 
 
     <!-- AUDIO EMBED -->
+    <span id="content-5"></span>
     <div class="background image-audio">
         <div class="overlay"></div>
         <div class="container margin-top-medium standard-padding">
@@ -271,7 +222,7 @@
                     <p class="caption-text bold-text">Tuesday 13 September 2016</p>
                     <p>Discover what it was like to wander the streets of Shakespeare’s London. Though large portions of the city from Shakespeare’s time have since been destroyed by fire, war and developers, a surprising number of buildings and places still survive. Author David Thomas discusses the sights, cuisine and pastimes of 16th century Londoners, while providing insight into what it was like to be a tourist during Shakespeare’s lifetime. Please note that there are occasional disruptions to the sound quality during this recording.</p>
 
-                    <a class="button-inverted" href="">Listen now</a>
+                    <a class="button-inverted" href="https://media.nationalarchives.gov.uk/index.php/tourists-guide-shakespeares-london/" target="_blank" rel="noopener noreferrer">Listen now</a>
                 </div><!-- end panel-center-->
             </div><!-- end row -->
         </div><!-- end container -->
@@ -286,9 +237,8 @@
         </div><!-- end row -->
     </div><!-- end container -->
 
-    <!-- SIMPLE BLOG EMBED -->
-
     <!-- RESOURCE EMBED -->
+    <span id="content-6"></span>
     <div class="background border-top-grey">
         <div class="container">
             <div class="row">
@@ -306,7 +256,7 @@
                 <h2>Paying for the privilege: a new Shakespeare discovery</h2>
                 <p class="caption-text bold-text">Saturday 23 April 2016</p>
                 <p class="margin-top-small"> One third of a pound does not go far today – it’s not even a small child’s pocket money. But in 1603, it helped Shakespeare secure his future.</p>
-                <a class="button-inverted" href="">Read the full blog</a>
+                <a class="button-inverted" href="https://blog.nationalarchives.gov.uk/paying-privilege-new-shakespeare-discovery/" target="_blank" rel="noopener noreferrer">Read the full blog</a>
             </div><!-- end panel-center-->
         </div><!-- end row -->
     </div><!-- end container -->
@@ -321,6 +271,7 @@
     </div><!-- end container -->
 
     <!-- RESOURCE EMBED -->
+    <span id="content-7"></span>
     <div class="background border-top-grey">
         <div class="container">
             <div class="row">
@@ -340,7 +291,7 @@
 
                 <ul class="research-links">
                     <li>
-                        <a href="">
+                        <a href="https://www.nationalarchives.gov.uk/education/resources/tudor-entertainment/" target="_blank" rel="noopener noreferrer">
                         <span class="heading">
                             Tudor Entertainment
                         </span>
@@ -350,7 +301,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="">
+                        <a href="https://www.nationalarchives.gov.uk/education/resources/james-i/" target="_blank" rel="noopener noreferrer">
                         <span class="heading">
                             James I
                         </span>
@@ -366,6 +317,7 @@
 
 
     <!-- BLOG EMBED -->
+    <span id="content-8"></span>
     <div class="background border-top-grey">
         <div class="container">
             <div class="row">
@@ -391,12 +343,13 @@
                         amendments, which was left in the probate court by his executor.
                     </p>
                 </div><!-- end blog embed text -->
-                <a class="button-inverted text-center" href="">Read the full blog</a>
+                <a class="button-inverted text-center" href="https://blog.nationalarchives.gov.uk/shakespeares-will-new-interpretation/" target="_blank" rel="noopener noreferrer">Read the full blog</a>
             </div><!-- end blog embed -->
         </div><!-- end row -->
     </div><!-- end container -->
 
     <!-- IMAGE VIEWER 2 -->
+    <span id="content-9"></span>
     <div class="row">
 
         <!-- PART 1: IMAGE -->
@@ -423,7 +376,7 @@
                 <p class="caption-text no-margin-bottom">Reference: PROB 1/4</p>
                 <p class="caption-text">Date created: 1616</p>
                <p>Excerpts from Shakespeare’s Will</p>
-                <a class="button" href="">View our classroom resource</a>
+                <a class="button" href="https://www.nationalarchives.gov.uk/education/resources/william-shakespeare/source-1/" target="_blank" rel="noopener noreferrer">View our classroom resource</a>
             </div><!-- end container -->
         </div><!-- end background -->
 
@@ -453,13 +406,14 @@
 
 
     <!-- RELATED LINKS COLLECTION -->
+    <span id="content-10"></span>
     <div class="background bg-black text-white">
         <div class="container related-content">
             <h2>Explore our other content about Shakespeare</h2>
                 <div class="row">
                     <div class="col-sm-12 col-md-6 col-lg-4">
                         <div class="card-group-secondary-nav">
-                            <a href='/' class="card-group-secondary-nav__link">
+                            <a href='https://blog.nationalarchives.gov.uk/kidnapped-order-child-actors-shakespeares-day/' class="card-group-secondary-nav__link" target="_blank" rel="noopener noreferrer">
                                 <h3 class="sr-only">Blog - Kidnapped to order: child actors in Shakespeare’s day</h3>
                                 <div class="card-group-secondary-nav__image">
                                     <picture>
@@ -481,7 +435,7 @@
                     </div><!-- end col -->
                     <div class="col-sm-12 col-md-6 col-lg-4">
                         <div class="card-group-secondary-nav">
-                            <a href='/' class="card-group-secondary-nav__link">
+                            <a href='https://media.nationalarchives.gov.uk/index.php/philip-henslowe-edward-alleyn-and-the-invention-of-london-theatre-in-the-age-of-shakespeare/' class="card-group-secondary-nav__link" target="_blank" rel="noopener noreferrer">
                                 <h3 class="sr-only">Podcast - Philip Henslowe, Edward Alleyn and the invention of London theatre in the age of Shakespeare</h3>
                                 <div class="card-group-secondary-nav__image">
                                     <picture>
@@ -504,7 +458,7 @@
                     </div><!-- end col -->
                     <div class="col-sm-12 col-md-6 col-lg-4">
                         <div class="card-group-secondary-nav">
-                            <a href='/' class="card-group-secondary-nav__link">
+                            <a href='https://www.eventbrite.co.uk/e/before-shakespeare-tickets-47213785704?aff=ebapi#' class="card-group-secondary-nav__link" target="_blank" rel="noopener noreferrer">
                                 <h3 class="sr-only">Event - ‘Before Shakespeare’ at The National Archives</h3>
                                 <div class="card-group-secondary-nav__image">
                                     <picture>
