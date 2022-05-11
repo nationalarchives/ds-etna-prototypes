79523bd14ed8806cfe8ee78eb0a99cd584a1436b
Matt Blair <7143978+matt-blair@users.noreply.github.com>
2022-05-11 18:18:21 +0100

    Amending version 6.1

--- details/v6_1/index.php
+++ details/v6_1/index.php
@@ -12,30 +12,30 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1 class="mb-5">Details page (version 6b) index</h1>
-                <p>Date created: 04 May 2022</p>
+                <h1 class="mb-5">Details page (version 6.1) index</h1>
+                <p>Date created: 10 May 2022</p>
                 <h2>TNA record example</h2>
                 <ul>
-                    <li><a href="/details/v6b/tna-record/item.html">Item</a></li>
+                    <li><a href="/details/v6_1/tna-record/item.html">Item</a></li>
                     <!--
-                    <li><a href="/details/v6b/tna-record/piece.html">Piece</a></li>
-                    <li><a href="/details/v6b/tna-record/sub-series.html">Sub-series</a></li>
-                    <li><a href="/details/v6b/tna-record/series.html">Series</a></li>
-                    <li><a href="/details/v6b/tna-record/division.html">Division</a></li>
-                    <li><a href="/details/v6b/tna-record/department.html">Department</a></li>
-                    <li><a href="/details/v6b/tna-record/archive.html">Archive</a></li>
+                    <li><a href="/details/v6_1/tna-record/piece.html">Piece</a></li>
+                    <li><a href="/details/v6_1/tna-record/sub-series.html">Sub-series</a></li>
+                    <li><a href="/details/v6_1/tna-record/series.html">Series</a></li>
+                    <li><a href="/details/v6_1/tna-record/division.html">Division</a></li>
+                    <li><a href="/details/v6_1/tna-record/department.html">Department</a></li>
+                    <li><a href="/details/v6_1/tna-record/archive.html">Archive</a></li>
                     -->
                 </ul>
                 <!--
                 <h2>Other archives example</h2>
                 <ul>
-                    <li><a href="/details/v6b/other-archives/myc-a2a.html">MYC/A2A</a></li>
-                    <li><a href="/details/v6b/other-archives/nra.html">NRA</a></li>
+                    <li><a href="/details/v6_1/other-archives/myc-a2a.html">MYC/A2A</a></li>
+                    <li><a href="/details/v6_1/other-archives/nra.html">NRA</a></li>
                 </ul>
 
                 <h2>Other pages</h2>
                 <ul>
-                    <li><a href="/details/v6b/other-pages/record-creators.html">Record creators</a></li>
+                    <li><a href="/details/v6_1/other-pages/record-creators.html">Record creators</a></li>
                 </ul>
 
                 -->
